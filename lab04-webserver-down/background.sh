ba274698074e43fd784528714cc22b7f345c2c2a
vishnusoerodikoro <44632142+vishnusoerodikoro@users.noreply.github.com>
2026-05-07 14:54:55 +0700

    Update background.sh

--- lab04-webserver-down/background.sh
+++ lab04-webserver-down/background.sh
@@ -4,12 +4,11 @@ apt-get install -y -qq nginx > /dev/null 2>&1
 systemctl enable nginx > /dev/null 2>&1
 systemctl start nginx > /dev/null 2>&1
 sleep 2
-
-# Inject Variant A: stop & disable nginx (most common real-world scenario)
+# Inject Variant A
 systemctl stop nginx > /dev/null 2>&1
 systemctl disable nginx > /dev/null 2>&1
 echo "variant-A" > /tmp/.lab04_variant
 echo "fault-injected" > /tmp/.lab04_ready
 
-# Auto-flush bash history setiap command
-echo 'export PROMPT_COMMAND="history -a"' >> /root/.bashrc
+# Auto-flush bash history
+echo 'export PROMPT_COMMAND="history -a"' > /etc/profile.d/histflush.sh
